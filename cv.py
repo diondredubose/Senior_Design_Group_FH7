60fd35a55083c592bb1198a99a436ff1a78332fd
diondredubose <60270033+diondredubose@users.noreply.github.com>
2023-01-27 20:36:26 -0600

    update

--- cv.py
+++ cv.py
@@ -3,6 +3,7 @@ import open3d as o3d
 import os
 import json
 import sys
+from PIL import Image
 
 pwd = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(pwd, '..'))
@@ -10,6 +11,65 @@ from initialize_config import initialize_config
 
 innie = r'C:\Users\benhu\Desktop\Test_Dataset' # where the file goes
 outie = r'C:\Users\benhu\Desktop\mkv_file\ecj_1202.mkv'## input file
+
+def squareimage(image):
+    # Open the image
+    im = Image.open(image)
+
+    # Get the aspect ratio
+    aspect_ratio = im.width / im.height
+
+    # Calculate the size of the square
+    size = 572
+
+    # Crop the image
+    if aspect_ratio > 1:
+        # Landscape image
+        left = (im.width - im.height) / 2
+        right = left + im.height
+        top = 0
+        bottom = im.height
+    else:
+        # Portrait image
+        left = 0
+        right = im.width
+        top = (im.height - im.width) / 2
+        bottom = top + im.width
+
+    im = im.crop((left, top, right, bottom))
+
+    # Resize the image to the square shape
+    im = im.resize((size, size))
+
+    # Save the image
+    im.save(image)
+
+def enter_folder(folder_name):
+    #gets current working directory (cwd)
+    cwd = os.getcwd()
+
+    #create folder path
+    folder_path = os.path.join(cwd, folder_name)
+
+    # go to directory specified by folder path
+    os.chdir(folder_path)
+
+    #prints the name of folder that is entered
+    print(" \n --- '{}' folder has been ENTERED --- \n".format(folder_name))
+
+def exit_folder():
+    #gets current working directory (cwd)
+    cwd = os.getcwd()
+
+    #path of parent directory (pd)
+    # os.path.dirname('/home/user/Documents/my_folder') would return /home/user/Documents
+    pd_path = os.path.dirname(cwd)
+
+    # go to directory specified by pd_path
+    os.chdir(pd_path)
+    
+    folder_name = cwd.rsplit('\\', 1)[1]
+    print(" \n --- '{}' folder has been EXITED --- \n".format(folder_name))
 
 class ReaderWithCallback:
 
@@ -78,13 +138,22 @@ class ReaderWithCallback:
                 if self.output is not None:
                     color_filename = '{0}/RGB_Images/frame_{1:05d}.jpg'.format(
                         self.output, idx)
+                    cwd = os.getcwd()
+                    
                     #print('Writing to {}'.format(color_filename))
                     o3d.io.write_image(color_filename, rgbd.color)
+                    #enter_folder("RGB_Images")
+                    squareimage(color_filename)
+                    #exit_folder()
 
                     depth_filename = '{0}/Depth_Maps/frame_{1:05d}.png'.format(
                         self.output, idx)
+                    
                     #print('Writing to {}'.format(depth_filename))
                     o3d.io.write_image(depth_filename, rgbd.depth)
+                    #enter_folder("Depth_Maps")
+                    squareimage(depth_filename)
+                    #exit_folder()
                     idx += 1
 
             try:
